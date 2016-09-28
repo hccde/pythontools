dfe522d4c4bad337d4263ef656103877ff9ac5d6
hccde <congconguse@gmail.com>
2016-09-28 18:04:03 +0800

    Unicode去除

--- automouse/mousemove.py
+++ automouse/mousemove.py
@@ -1,6 +1,6 @@
 from pymouse import PyMouse
 m = PyMouse()
-m.position()#获取当前坐标的位置
-m.move(x,y)#鼠标移动到xy位置
-m.click(x,y)#移动并且在xy位置点击
-m.click(x,y,1|2)#移动并且在xy位置点击,左右键点击
+m.position()
+m.move(0,0)
+# m.click(x,y)
+# m.click(x,y,1|2)
