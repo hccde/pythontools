9022c985e2e3e1246c02bc5509aeeb3317ce36f5
hccde <1076663958@qq.com>
2017-02-28 00:10:06 +0800

    max request = 10

--- spider-for-SIPO/index.py
+++ spider-for-SIPO/index.py
@@ -7,7 +7,7 @@ from pyquery import PyQuery as pq
 def get_configure(file_path):
 	with open(file_path, 'r') as f:
 		data = json.load(f)
-		print(data)
+		return data
 
 async def fetch(session, url):
     with async_timeout.timeout(10):
@@ -23,6 +23,6 @@ async def main(loop):
 #     async with session.get('https://api.github.com/events') as resp:
 #         print(resp.status)
 #         print(await resp.text())
-get_configure('./configure.json');
+get_configure('./configure.json')
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main(loop))
