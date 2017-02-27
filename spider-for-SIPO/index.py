f602a2943bd66eb05dbc5b9e358d2b0e83b35052
hccde <1076663958@qq.com>
2017-02-27 23:32:54 +0800

    python

--- spider-for-SIPO/index.py
+++ spider-for-SIPO/index.py
@@ -1,6 +1,13 @@
 import aiohttp
 import asyncio
 import async_timeout
+import json
+from pyquery import PyQuery as pq
+
+def get_configure(file_path):
+	with open(file_path, 'r') as f:
+		data = json.load(f)
+		print(data)
 
 async def fetch(session, url):
     with async_timeout.timeout(10):
@@ -12,5 +19,10 @@ async def main(loop):
         html = await fetch(session, 'http://python.org')
         print(html)
 
+# async with aiohttp.ClientSession() as session:
+#     async with session.get('https://api.github.com/events') as resp:
+#         print(resp.status)
+#         print(await resp.text())
+get_configure('./configure.json');
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main(loop))
