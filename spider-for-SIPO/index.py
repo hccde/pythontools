c7023995584586936c35ef9bdcab6cedee67bcf8
hccde <1076663958@qq.com>
2017-03-01 11:40:47 +0800

    proxy

--- spider-for-SIPO/index.py
+++ spider-for-SIPO/index.py
@@ -3,6 +3,8 @@ import asyncio
 import async_timeout
 import json
 import copy 
+import requests
+from bs4 import BeautifulSoup
 from pyquery import PyQuery as pq
 
 def get_configure(file_path):
@@ -10,9 +12,13 @@ def get_configure(file_path):
 		data = json.load(f)
 		return data
 def write_file(string):
-    fs = open('data', 'w')
+    fs = open('dest', 'a')
+    log = open('configure.json','w')
     fs.write(string)
+    log.write(json.dumps(configure))
+    log.close();
     fs.close()
+
 
 def get_file(path):
     fs = open(path,'r')
@@ -23,37 +29,53 @@ def get_info(str):
     string = '';
     for index in range(0,length) :
         string+=q('.item-content-body').eq(index).text()+'\n\n'
+    write_file(string)
     print(string)
 
+def get_proxy():
+    proxys = BeautifulSoup(requests.get("http://qsrdk.daili666api.com/ip/?tid=557761112430648&num=1").text).p.contents[0]
+    print proxys
+    return proxys.strip();
+
+
 async def fetch(session, url,params):
-    with async_timeout.timeout(100):
+    with async_timeout.timeout(20):
         async with session.post(url,data=params) as response:
-            return await response.text()
+            text = await response.text()
+            if len(text)<300:
+                proxy = get_proxy()
+                configure['params']['resultPagination.start']-=configure['params']['resultPagination.limit']
+            else:
+                return text
+            return '';
 
 async def request(loop,params):
-    async with aiohttp.ClientSession(loop=loop) as session:
+    async with aiohttp.ClientSession(loop=loop,proxy='http://'+proxy) as session:
         html = await fetch(session,configure['url'],params)
         #when return ,check queue,construct param,
-        write_file(html)
-        print(html)
+        get_info(html)
 
-# async with aiohttp.ClientSession() as session:
-#     async with session.get('https://api.github.com/events') as resp:
-#         print(resp.status)
-#         print(await resp.text())
+
 configure = get_configure('./configure.json')
 
 def tasks_group(config):
     maxRequest = configure['maxRequest']
     tasks = []
     for index in range(0,maxRequest):
-        print('request url')
         loop = asyncio.get_event_loop()
         params = copy.deepcopy(configure['params'])
         tasks.append(loop.create_task(request(loop,params)))
         configure['params']['resultPagination.start']+=configure['params']['resultPagination.limit'];        
     loop.run_until_complete(asyncio.wait(tasks));
-    # configure['current'] +=configure.current+maxRequest;
 
 # tasks_group(configure)
-get_info(get_file('data'))
+# get_info(get_file('data'))
+def main():
+    end = configure['params']['resultPagination.totalCount']
+    start = configure['params']['resultPagination.start']
+    while end >= start:
+        tasks_group(configure)
+    print('this city ok')
+
+proxy = get_proxy();
+main();
---
+++ spider-for-SIPO/readme.md
@@ -0,0 +1,8 @@
+要求 python3.5 以上
+
+pip install -r requirements.txt 安装依赖
+
+configure.json 是抓取请求的信息，resultPagination.start 递增
+请求的信息可从浏览器中获得
+
+python index.py
