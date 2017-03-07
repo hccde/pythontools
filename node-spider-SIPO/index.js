9712571c5dde40e08fac8784d94431e88e866a83
hccde <1076663958@qq.com>
2017-03-07 21:22:28 +0800

    nodejs spider & gitignore

---
+++ node-spider-SIPO/index.js
@@ -0,0 +1,13 @@
+let request = require('request'),
+	cheerio=require('cheerio');
+
+let req_info = require('./req_info.json');
+
+console.log(req_info)
+function main(){
+
+}
+
+function req(){
+
+}
