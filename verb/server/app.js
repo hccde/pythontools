82d410c7811429702b766dd85d7bbf95804d1f34
hccde <1076663958@qq.com>
2017-05-15 18:52:00 +0800

    static files

--- verb/server/app.js
+++ verb/server/app.js
@@ -2,10 +2,19 @@
 
 let express = require('express');
 let router = require('./routers/index');
+let morgan = require('morgan');
+let fs = require('fs');
+let path = require('path');
 let app = express();
 
 //router
 app.use('/',router);
+//log
+let  accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+app.use(morgan('combined',{
+	stream:accessLogStream
+}));
+
 
 //start server
 let server = app.listen(3030,()=>{
---
+++ verb/static/js/index.js
@@ -0,0 +1,7 @@
+let el = document.getElementById('input-word');
+el.addEventListener('input',deteChange);
+
+function deteChange(event){
+	console.log(event)
+}
+//ajax
