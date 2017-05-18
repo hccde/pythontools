ef458ead9f300684c309835eb1c41907951326c6
hccde <1076663958@qq.com>
2017-05-18 18:45:38 +0800

    style

--- verb/static/js/index.js
+++ verb/static/js/index.js
@@ -2,6 +2,6 @@ let el = document.getElementById('input-word');
 el.addEventListener('input',deteChange);
 
 function deteChange(event){
-	console.log(event)
+	let value = event.target.value;
+	console.log(event.target.value)
 }
-//ajax
