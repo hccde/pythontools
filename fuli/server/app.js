c3ae93e98728f55a41e59313954918a49e13ac0b
hccde <1076663958@qq.com>
2018-03-09 23:08:28 +0800

    save

--- fuli/server/app.js
+++ fuli/server/app.js
@@ -1,22 +1,19 @@
 let express = require('express');
 let fs = require('fs');
 let str = fs.readFileSync('./index.html').toString();
-let list = fs.readdirSync('../dist');
-console.log(list)
+let list = fs.readdirSync('/Users/admos/Documents/notchild/VideoData');
 app = express();
 
 app.use(express.static('public'));
 app.use(express.static('../server'));
+app.use(express.static('/Users/admos/Documents/notchild/VideoData/'));
 
-app.get('/', function(req, res){
-    res.send(str);
-});
 app.get('/list',function(req, res){
     console.log(req.query);
     let { page,pageSize} = req.query;
     res.send({
         code:200,
-        list:[]
+        list:list
     })
 })
 
--- fuli/server/index.js
+++ fuli/server/index.js
@@ -1,13 +1,30 @@
 
-var el = document.getElementById('app');
-
+var list = [];
+var index = 0;
+var page = 1;
+function getList(page){
 $.ajax({
     url:"/list",
     data: {
-        page:1,
+        page:page,
         pageSize:20
     },
     success:function(res){
-        console.log(res)
+        list = res.list;
+        index = 1;
     }
 });
+}
+getList(1);
+$('#button').click(function(){
+    $('#video').attr('src',list[index])
+    window.index=window.index+1;
+    if(window.index >= 20){
+        page+=1;
+        getList(page);
+    }
+});
+$('#submit').click(function(){
+    var  p = $("#input").val();
+    getList(p);
+})
